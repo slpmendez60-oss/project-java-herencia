00bcd46c86252dfcafb546e98aa2a446d061c9e0
Luis Mendez <slpmendez60@gmail.com>
2025-12-05 21:29:29 -0500

    Implementamos la subclase SistemaFrenos y la logica Main

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        Motor motor = new Motor("M001", "Bosch", 4, 120);
+        Transmision transmision = new Transmision("T001", "ZF", "Automática", 6);
+        Neumatico neumatico = new Neumatico("N001", "Michelin", 17, 32.5);
+        Chasis chasis = new Chasis("C001", "Toyota", "Monocasco", 250.0);
+        SistemaFrenos frenos = new SistemaFrenos("F001", "Brembo", true, "Discos ventilados");
+
+        System.out.println("==== MOTOR ====");
+        motor.mostrarInformacion();
+        motor.encenderMotor();
+
+        System.out.println("\n==== TRANSMISIÓN ====");
+        transmision.mostrarInformacion();
+        transmision.cambiarMarcha(3);
+
+        System.out.println("\n==== NEUMÁTICO ====");
+        neumatico.mostrarInformacion();
+        neumatico.verificarPresion();
+
+        System.out.println("\n==== CHASIS ====");
+        chasis.mostrarInformacion();
+        chasis.calcularCargaMaxima();
+
+        System.out.println("\n==== SISTEMA DE FRENOS ====");
+        frenos.mostrarInformacion();
+        frenos.verificarABS();
+    }
+}
