fda09c6a3c7d73a7d6c2a357d6113b9fe3b25957
Luis Mendez <slpmendez60@gmail.com>
2025-12-05 21:14:44 -0500

    Implementamos la clase base ComponenteVehicular

---
+++ src/ComponenteVehicular.java
@@ -0,0 +1,15 @@
+public class ComponenteVehicular {
+    String codigo;
+    String fabricante;
+
+    public ComponenteVehicular(String codigo, String fabricante) {
+        this.codigo = codigo;
+        this.fabricante = fabricante;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Código: " + codigo);
+        System.out.println("Fabricante: " + fabricante);
+    }
+}
+
